50cae4ade18db045d3a1176861386bf858eb43dd
Hoàng Minh Thắng <p@banphim.net>
2013-09-06 19:15:33 +0700

    remove footer links

--- footer.php
+++ footer.php
@@ -10,9 +10,7 @@
  echo stripslashes($footer_content);
 ?>
 </p>
-</div><h2 style="position: absolute;display:block; text-indent:-9999px; top: -100px; overflow: auto"><strong><em>
-<a href="http://mualanhay.com/news/Dich-vu-mua-lan/">dịch vụ múa lân</a> 
-</em></strong></h2>
+</div>
     </div>
     <div class="art-footer-background">
     </div>
@@ -20,13 +18,11 @@
 
 		<div class="cleared"></div>
     </div>
-</div><h2 style="position: absolute;display:block; text-indent:-9999px; top: -100px; overflow: auto"><strong><em>
-<a href="http://mualanhay.com/news/Mua-lan-su-rong/Mua-lan-su-rong-la-gi-229/">múa lân sư rồng</a> 
-</em></strong></h2>
+</div>
 <div class="cleared"></div>
-<p class="art-page-footer">Design: <a href="http://www.free-wordpress-theme.net" target="_blank">Free Wordpress Themes</a> | <?php 
+<?php 
 /* This theme is powered by free-wordpress-theme.net, please do NOT remove the comment or anything below. */
-			wp_theme_GPL_credits();
+			//wp_theme_GPL_credits();
 /* This theme is powered by free-wordpress-theme.net, please do NOT remove the comment or anything below. */ ?></p>
 
 <!-- <?php printf(__('%d queries. %s seconds.', 'kubrick'), get_num_queries(), timer_stop(0, 3)); ?> -->
